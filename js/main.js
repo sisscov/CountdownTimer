7b943919d7bd92a3d3e506961c1a36b69329f216
Julia Banaszek <julia.m.banaszek@gmail.com>
2022-08-09 17:37:36 +0200

    Handling lack of input, visual changes

--- js/main.js
+++ js/main.js
@@ -1,13 +1,24 @@
 function CalcTime(dates) {
-  getEventName();
   clearInterval();
 
   if (dates != "") {
     date = new Date(dates).getTime();
+    localStorage.setItem("info", date);
   } else {
     alert("Please enter the date of the event");
     CountDown.preventDefault();
   }
+  //given date is past
+  let now = new Date().getTime();
+  if (date < now) {
+    alert(
+      "We cannot go back in time :( Please enter a valid date of the event"
+    );
+    CountDown.preventDefault();
+    GetEventName.preventDefault();
+  }
+
+  GetEventName();
 
   //time calculation and text replacement
   function CountDown() {
@@ -22,14 +33,25 @@ function CalcTime(dates) {
     document.querySelector(".hour").innerHTML = hour;
     document.querySelector(".minute").innerHTML = minute;
     document.querySelector(".second").innerHTML = second;
+
+    if (now >= date) {
+      clearInterval();
+      document.querySelector(".day").innerHTML = "0";
+      document.querySelector(".hour").innerHTML = "0";
+      document.querySelector(".minute").innerHTML = "0";
+      document.querySelector(".second").innerHTML = "0";
+
+      FinishedEvent();
+    }
   }
+
   setInterval(function () {
     CountDown();
   }, 1000);
 }
 
 //checking the name of the event
-function getEventName() {
+function GetEventName() {
   let eventName = document.getElementById("name_event").value;
   if (eventName == "") {
     alert("Please enter the name of the event");
@@ -38,4 +60,9 @@ function getEventName() {
     document.getElementById("time_left").innerHTML =
       "Time left until " + eventName + ":";
   }
+}
+function FinishedEvent() {
+  let eventName = document.getElementById("name_event").value;
+  document.getElementById("time_left").innerHTML =
+    "It's time for " + eventName + "!!!";
 }
